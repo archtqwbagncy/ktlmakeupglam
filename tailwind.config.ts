fb0f4590a1cd56125eb8625cbdeae0a740e10f4c
lmvtrdngent <your-email@example.com>
2026-04-03 19:27:30 +0200

    Update entire website fonts: Headings to TAN Mon Cheri and body text to Agrandir Grand.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ export default {
       },
     },
     extend: {
+      fontFamily: {
+        heading: ["TAN Mon Cheri", "serif"],
+        sans: ["Agrandir Grand", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
